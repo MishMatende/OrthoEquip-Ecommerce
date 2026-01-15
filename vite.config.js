d8e0153b7d88b988ea1c2eb04e8857fcb5c63829
mishaelmatende <mishaelmatende@gmail.com>
2026-01-15 13:37:11 +0300

    I hope this won't crash the website

--- vite.config.js
+++ vite.config.js
@@ -3,15 +3,13 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import path from "path";
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: "/", // <- ADD THIS
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  css: {
-    postcss: "../postcss.config.js",
-  },
+  css: {},
 });
